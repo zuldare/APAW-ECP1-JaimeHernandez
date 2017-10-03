89ee7647ab01b34ad7b06f33f7c93b81f3d9a115
Jaime Hernández Ortiz <jaimehernandezortiz@gmail.com>
2017-10-04 00:35:49 +0200

    Fixed bugs

--- src/test/java/patterns/AllPatternTests.java
+++ src/test/java/patterns/AllPatternTests.java
@@ -1,4 +1,4 @@
-package forge;
+package patterns;
 
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
@@ -6,11 +6,13 @@ import org.junit.runners.Suite.SuiteClasses;
 
 import builder.TaskBuilderTest;
 import builder.UserBuilderTest;
+import composite.TaskCompositeTest;
 
 @RunWith(Suite.class)
 @SuiteClasses({
     TaskBuilderTest.class,
-    UserBuilderTest.class
+    UserBuilderTest.class,
+    TaskCompositeTest.class
 })
 public class AllPatternTests {
 
